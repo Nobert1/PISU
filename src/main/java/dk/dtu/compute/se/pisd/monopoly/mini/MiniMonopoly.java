a15419210a7ba51188724fb7307654b6005afee6
Gustav <gustav.nobert1998@gmail.com>
2019-04-30 14:15:23 +0200

    possible fix for bois?

--- src/main/java/dk/dtu/compute/se/pisd/monopoly/mini/MiniMonopoly.java
+++ src/main/java/dk/dtu/compute/se/pisd/monopoly/mini/MiniMonopoly.java
@@ -418,10 +418,6 @@ public class MiniMonopoly {
             createSpaces(game);
 		    controller.databaseinteraction();
             game.shuffleCardDeck();
-<<<<<<< HEAD
-=======
-            //createPlayers(game);
->>>>>>> trade_method
             controller.play();
         } catch (DALException e) {
 		    e.getMessage();
